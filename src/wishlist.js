f486ddf34cbab1100f484fdd78d27c1d1f766be0
masatoarai <sekkithub@gmail.com>
2019-05-25 14:10:58 +0100

    Updating

--- src/wishlist.js
+++ src/wishlist.js
@@ -63,6 +63,7 @@ export default () => {
         return response;
       })
       .catch((error) => {
+        console.log(error);
         return error;
       });
   }
@@ -102,6 +103,7 @@ export default () => {
         return response;
       })
       .catch((error) => {
+        console.log(error);
         return error;
       });
   }
@@ -117,10 +119,11 @@ export default () => {
       },
     })
       .then((response) => {
-        callback(response);
+        callback(response, customerId);
         return response;
       })
       .catch((error) => {
+        console.log(error);
         return error;
       });
   }
@@ -129,7 +132,7 @@ export default () => {
   /**
    * Set Header Icon.
    */
-  function updateHeaderIcon() {
+  function updateNavHeartStatus() {
     const customerId = theme.customer.id;
     getList(customerId, (response) => {
       const totalItems = response.data.resultsCount;
@@ -144,7 +147,7 @@ export default () => {
   /**
    * Filled a heart icon if the item is on Wishlist List.
    */
-  function updateHeartIcons() {
+  function updateAddButtonsHeartStatus() {
     const customerId = theme.customer.id;
 
     if (!customerId) {
@@ -156,12 +159,12 @@ export default () => {
       const productArray = [];
 
       products.forEach((product) => {
-        const productId = product.id.replace(/gid:\/\/shopify\/Product\//g, '');
+        const productId = Number(product.id.replace(/gid:\/\/shopify\/Product\//g, ''));
         productArray.push(productId);
       });
 
       nodeSelectors.addButtons.forEach((button) => {
-        const productId = button.getAttribute('data-product-id');
+        const productId = Number(button.getAttribute('data-product-id'));
         if (productArray.indexOf(productId) > -1) {
           button.classList.add('is-filled');
         } else {
@@ -195,8 +198,8 @@ export default () => {
       }
 
       removeItem(customerId, productId, () => {
-        updateHeaderIcon();
-        updateHeartIcons();
+        updateNavHeartStatus();
+        updateAddButtonsHeartStatus();
         updateList(productId);
         setFallbackMessage();
       });
@@ -223,7 +226,7 @@ export default () => {
   function handleAddEvent(event) {
     const target = event.currentTarget;
     const customerId = theme.customer.id;
-    const productId = target.getAttribute('data-product-id');
+    const productId = Number(target.getAttribute('data-product-id'));
 
     lockButton(target);
 
@@ -235,15 +238,14 @@ export default () => {
     if (target.classList.contains('is-filled')) {
       target.classList.remove('is-filled');
       removeItem(customerId, productId, (target) => {
-        updateHeaderIcon();
+        updateNavHeartStatus();
         unlockButton(target);
-        return response;
       }, target);
     } else {
       target.classList.add('is-filled');
       addItem(customerId, productId, (target) => {
-        updateHeaderIcon();
-        updateHeartIcons();
+        updateNavHeartStatus();
+        updateAddButtonsHeartStatus();
         unlockButton(target);
       }, target);
     }
@@ -256,7 +258,7 @@ export default () => {
     const target = event.currentTarget;
     const item = target.closest(selectors.item);
     const customerId = theme.customer.id;
-    const productId = target.getAttribute('data-product-id');
+    const productId = Number(target.getAttribute('data-product-id'));
 
     lockButton(target);
     item.classList.add('is-removing');
@@ -266,7 +268,7 @@ export default () => {
     }
 
     removeItem(customerId, productId, () => {
-      updateHeaderIcon();
+      updateNavHeartStatus();
       updateList(productId);
       setFallbackMessage();
     }, target);
@@ -322,75 +324,79 @@ export default () => {
   /**
    * Construct Wishlist.
    */
-  function constructList() {
+  function constructList(response, customerId) {
+    const products = response.data.results.data.nodes;
+    const ul = document.createElement('ul');
+    ul.classList.add('row', 'wishlist__list');
+    ul.setAttribute('js-wishlist', 'list');
+    nodeSelectors.loading.parentNode.removeChild(nodeSelectors.loading);
+    nodeSelectors.container.appendChild(ul);
+
+    products.forEach((product) => {
+
+      const id = product.id.replace(/gid:\/\/shopify\/Product\//g, '');
+      const productId = Number(id);
+      const handle = product.handle;
+      const link = `/products/${handle}`;
+      const featuredImage = product.imageS.transformedSrc;
+      const title = product.title;
+
+      const li = document.createElement('li');
+      li.setAttribute('data-product-id', productId);
+      li.setAttribute('js-wishlist', 'item');
+      li.classList.add('col', 's6', 'm4', 'l3', 'wishlist__item');
+      ul.appendChild(li);
+      li.innerHTML += `
+        <div class="wishlist-card">
+          <a href="${link}">
+            <div class="wishlist-card__thumbnail-container">
+              <img src="${featuredImage}" class="wishlist-card__thumbnail-image" />
+
+              <div
+                class="button button--primary button--block button--small wishlist-card__button"
+                js-quick-view="toggle"
+                data-product-url="/products/${handle}">
+                Quickview
+              </div>
+            </div>
+          </a>
+      
+          <div class="wishlist-card__footer">
+            <a class="wishlist-card__link" href="${link}">
+              ${title}
+            </a>
+          </div>
+
+          <div
+            class="wishlist-button wishlist-button--remove"
+            js-wishlist="remove"
+            data-product-id="${productId}"
+            data-customer-id="${customerId}">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon__close" viewBox="0 0 36 36">
+              <path d="M12.51 8.46l-3.17 3.17 1.62 1.62 4 4.08-4 4-1.62 1.55 3.17 3.24 1.62-1.62 4.07-4.07 4.01 4.07 1.55 1.62L27 22.88l-1.62-1.55-4.07-4 4.07-4.08L27 11.63l-3.24-3.17-1.55 1.62-4.01 4.01-4.07-4.01-1.62-1.62z"></path>
+            </svg>
+          </div>
+        </div>
+      `;
+    });
+  }
+
+  /**
+   * Set Wishlist.
+   */
+  function setList() {
     if (!nodeSelectors.container) {
       return;
     }
 
-    const customerId = nodeSelectors.container.getAttribute('data-customer-id');
+    const customerId = theme.customer.id;
 
-    getList(customerId, (response) => {
-      const products = response.data.results.data.nodes;
-      const ul = document.createElement('ul');
-      ul.classList.add('row', 'wishlist__list');
-      ul.setAttribute('js-wishlist', 'list');
-      nodeSelectors.loading.parentNode.removeChild(nodeSelectors.loading);
-      nodeSelectors.container.appendChild(ul);
-
-      products.forEach((productId) => {
-        const id = productId.id.replace(/gid:\/\/shopify\/Product\//g, '');
-        const handle = productId.handle;
-        const link = `/products/${handle}`;
-        const featuredImage = productId.imageS.transformedSrc;
-        const title = productId.title;
-
-        const li = document.createElement('li');
-        li.setAttribute('data-product-id', id);
-        li.setAttribute('js-wishlist', 'item');
-        li.classList.add('col', 's6', 'm4', 'l3', 'wishlist__item');
-        ul.appendChild(li);
-        li.innerHTML += `
-          <div class="wishlist-card">
-            <a href="${link}">
-              <div class="wishlist-card__thumbnail-container">
-                <img src="${featuredImage}" class="wishlist-card__thumbnail-image" />
-
-                <div
-                  class="button button--primary button--block button--small wishlist-card__button"
-                  js-quick-view="toggle"
-                  data-product-url="/products/${handle}">
-                  Quickview
-                </div>
-              </div>
-            </a>
-        
-            <div class="wishlist-card__footer">
-              <a class="wishlist-card__link" href="${link}">
-                ${title}
-              </a>
-            </div>
-
-            <div
-              class="wishlist-button wishlist-button--remove"
-              js-wishlist="remove"
-              data-product-id="${id}"
-              data-customer-id="${customerId}">
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon__close" viewBox="0 0 36 36">
-                <path d="M12.51 8.46l-3.17 3.17 1.62 1.62 4 4.08-4 4-1.62 1.55 3.17 3.24 1.62-1.62 4.07-4.07 4.01 4.07 1.55 1.62L27 22.88l-1.62-1.55-4.07-4 4.07-4.08L27 11.63l-3.24-3.17-1.55 1.62-4.01 4.01-4.07-4.01-1.62-1.62z"></path>
-              </svg>
-            </div>
-          </div>
-        `;
-      });
-
+    getList(customerId, (response, customerId) => {
+      constructList(response, customerId);
       setRemoveEventHandlers();
 
-      if (isItemInWishlist(ul)) {
-        return;
-      }
-
-      showFallbackMessage();
-      updateHeaderIcon();
+      setFallbackMessage();
+      updateNavHeartStatus();
     });
   }
 
@@ -398,19 +404,18 @@ export default () => {
    * Initialise component.
    */
   function init() {
-    updateHeaderIcon();
-    updateHeartIcons();
+    updateNavHeartStatus();
+    updateAddButtonsHeartStatus();
     setEventListeners();
     setAddEventHandlers();
-    setRemoveEventHandlers();
-    constructList();
+    setList();
   }
 
   /**
    * Expose public interface.
    */
   return Object.freeze({
-    updateHeartIcons,
+    updateAddButtonsHeartStatus,
     setAddEventHandlerInQuickView,
     init,
   });
